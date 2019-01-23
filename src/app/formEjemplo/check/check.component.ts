b36f687c1f9dceba0320ac83fe5f8991abc973ef
jesusvalderramaanadat <jesusvalderramaanadat@gmail.com>
2019-01-23 17:08:43 +0100

    pruebas check dinamico

--- src/app/formEjemplo/check/check.component.ts
+++ src/app/formEjemplo/check/check.component.ts
@@ -24,25 +24,17 @@ export class CheckComponent implements OnInit {
   }
 
   ngOnInit() {
-    const controls = this.orders.map(c => new FormControl(false));
+    const controls = this.orders.map(c => {
+    if (true) {
+      return new FormControl(true);
+    }
+    });
+
     this.form = this.formBuilder.group({
       orders: new FormArray(controls)
     });
+
+    this.form.get('orders').get('1').setValue(false);
   }
 
-}
-
-function minSelectedCheckboxes(min = 1) {
-  const validator: ValidatorFn = (formArray: FormArray) => {
-    const totalSelected = formArray.controls
-      // get a list of checkbox values (boolean)
-      .map(control => control.value)
-      // total up the number of checked checkboxes
-      .reduce((prev, next) => next ? prev + next : prev, 0);
-
-    // if the total is not greater than the minimum, return the error message
-    return totalSelected >= min ? null : { required: true };
-  };
-
-  return validator;
 }
